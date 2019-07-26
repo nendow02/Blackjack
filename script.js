bd486161d51b4eda6b3a5d97d249d17b047340dd
summer <summer@summers-MacBook-Pro-5.local>
2019-07-26 13:41:12 -0700

    my first github repo

---
+++ script.js
@@ -0,0 +1,343 @@
+let aceClub = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Playing_card_club_A.svg/1024px-Playing_card_club_A.svg.png"
+let twoClub = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Playing_card_club_2.svg/1024px-Playing_card_club_2.svg.png"
+let threeClub = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Playing_card_club_3.svg/1024px-Playing_card_club_3.svg.png"
+let fourClub =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Playing_card_club_4.svg/1024px-Playing_card_club_4.svg.png"
+let fiveClub="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_club_5.svg/1024px-Playing_card_club_5.svg.png"
+let sixClub="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Playing_card_club_6.svg/1024px-Playing_card_club_6.svg.png"
+let sevenClub="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/Playing_card_club_7.svg/1024px-Playing_card_club_7.svg.png"
+let eightClub="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Playing_card_club_8.svg/1024px-Playing_card_club_8.svg.png"
+let nineClub = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Playing_card_club_9.svg/1024px-Playing_card_club_9.svg.png"
+let tenClub="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Playing_card_club_10.svg/1024px-Playing_card_club_10.svg.png"
+let jackClub="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Playing_card_club_J.svg/1024px-Playing_card_club_J.svg.png"
+let queenClub="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Playing_card_club_Q.svg/1024px-Playing_card_club_Q.svg.png"
+let kingClub="https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Playing_card_club_K.svg/1024px-Playing_card_club_K.svg.png"
+let aceDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Playing_card_diamond_A.svg/1024px-Playing_card_diamond_A.svg.png"
+let twoDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Playing_card_diamond_2.svg/1024px-Playing_card_diamond_2.svg.png"
+let threeDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Playing_card_diamond_3.svg/1024px-Playing_card_diamond_3.svg.png"
+let fourDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Playing_card_diamond_4.svg/1024px-Playing_card_diamond_4.svg.png"
+let fiveDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Playing_card_diamond_5.svg/1024px-Playing_card_diamond_5.svg.png"
+let sixDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Playing_card_diamond_6.svg/1024px-Playing_card_diamond_6.svg.png"
+let sevenDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Playing_card_diamond_7.svg/1024px-Playing_card_diamond_7.svg.png"
+let eightDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Playing_card_diamond_8.svg/1024px-Playing_card_diamond_8.svg.png"
+let nineDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Playing_card_diamond_9.svg/1024px-Playing_card_diamond_9.svg.png"
+let tenDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Playing_card_diamond_10.svg/1024px-Playing_card_diamond_10.svg.png"
+let jackDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Playing_card_diamond_J.svg/1024px-Playing_card_diamond_J.svg.png"
+let queenDiamond="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/Playing_card_diamond_Q.svg/1024px-Playing_card_diamond_Q.svg.png"
+let kingDiamond= "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Playing_card_diamond_K.svg/1024px-Playing_card_diamond_K.svg.png"
+let aceHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Playing_card_heart_A.svg/1024px-Playing_card_heart_A.svg.png"
+let twoHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Playing_card_heart_2.svg/1024px-Playing_card_heart_2.svg.png"
+let threeHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Playing_card_heart_3.svg/1024px-Playing_card_heart_3.svg.png"
+let fourHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Playing_card_heart_4.svg/1024px-Playing_card_heart_4.svg.png"
+let fiveHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_heart_5.svg/1024px-Playing_card_heart_5.svg.png"
+let sixHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Playing_card_heart_6.svg/1024px-Playing_card_heart_6.svg.png"
+let sevenHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Playing_card_heart_7.svg/1024px-Playing_card_heart_7.svg.png"
+let eightHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_heart_8.svg/1024px-Playing_card_heart_8.svg.png"
+let nineHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Playing_card_heart_9.svg/1024px-Playing_card_heart_9.svg.png"
+let tenHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Playing_card_heart_10.svg/1024px-Playing_card_heart_10.svg.png"
+let jackHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Playing_card_heart_J.svg/1024px-Playing_card_heart_J.svg.png"
+let queenHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Playing_card_heart_Q.svg/1024px-Playing_card_heart_Q.svg.png"
+let kingHeart="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Playing_card_heart_K.svg/1024px-Playing_card_heart_K.svg.png"
+let aceSpade = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Playing_card_spade_A.svg/1024px-Playing_card_spade_A.svg.png"
+let twoSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Playing_card_spade_2.svg/1024px-Playing_card_spade_2.svg.png"
+let threeSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Playing_card_spade_3.svg/1024px-Playing_card_spade_3.svg.png"
+let fourSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Playing_card_spade_4.svg/1024px-Playing_card_spade_4.svg.png"
+let fiveSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Playing_card_spade_5.svg/1024px-Playing_card_spade_5.svg.png"
+let sixSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Playing_card_spade_6.svg/1024px-Playing_card_spade_6.svg.png"
+let sevenSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Playing_card_spade_7.svg/1024px-Playing_card_spade_7.svg.png"
+let eightSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Playing_card_spade_8.svg/1024px-Playing_card_spade_8.svg.png"
+let nineSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Playing_card_spade_9.svg/1024px-Playing_card_spade_9.svg.png"
+let tenSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Playing_card_spade_10.svg/1024px-Playing_card_spade_10.svg.png"
+let jackSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/Playing_card_spade_J.svg/1024px-Playing_card_spade_J.svg.png"
+let queenSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Playing_card_spade_Q.svg/1024px-Playing_card_spade_Q.svg.png"
+let kingSpade="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Playing_card_spade_K.svg/1024px-Playing_card_spade_K.svg.png"
+let cardLinks = [aceClub,twoClub,threeClub,fourClub,fiveClub,sixClub,sevenClub,eightClub,nineClub,tenClub,jackClub,queenClub,kingClub,aceDiamond,twoDiamond,threeDiamond,fourDiamond,fiveDiamond,sixDiamond,sevenDiamond,eightDiamond,nineDiamond,tenDiamond,jackDiamond,queenDiamond,kingDiamond,aceHeart,twoHeart,threeHeart,fourHeart,fiveHeart,sixHeart,sevenHeart,eightHeart,nineHeart,tenHeart,jackHeart,queenHeart,kingHeart,aceSpade,twoSpade,threeSpade,fourSpade,fiveSpade,sixSpade,sevenSpade,eightSpade,nineSpade,tenSpade,jackSpade,queenSpade,kingSpade];
+
+var money = 100
+var dpulledCards = []
+var pPulledCards = []
+var dScore = 0
+var pScore = 0
+var bet = 0
+var aceUsed = []
+var extra = 0
+var extraD = 0
+
+function cardPull(i) {
+  let max = 51
+  var random = 0
+  function pull() {
+     random = Math.floor(Math.random() * (max)) + 1; 
+  if (dPulledCards.includes(random) || pPulledCards.includes(random)) {
+    pull()
+  }
+}
+  pull();
+  if (i == "d") {
+  dPulledCards.push(random);
+  } else {
+    pPulledCards.push(random)
+  }
+  return random;
+}
+
+function cardPoints() {
+  if (currentCard == 0 || currentCard == 13 || currentCard == 26 || currentCard == 39) {
+    return 11
+  }
+  else if (currentCard == 1 || currentCard == 14 || currentCard == 27 || currentCard == 40) {
+    return 2
+  }
+   else if (currentCard == 2 || currentCard == 15 || currentCard == 28 || currentCard == 41) {
+    return 3
+  }
+   else if (currentCard == 3 || currentCard == 16 || currentCard == 29 || currentCard == 42) {
+    return 4
+  }
+   else if (currentCard == 4 || currentCard == 17 || currentCard == 30 || currentCard == 43) {
+    return 5
+  }
+   else if (currentCard == 5 || currentCard == 18 || currentCard == 31 || currentCard == 44) {
+    return 6
+  }
+   else if (currentCard == 6 || currentCard == 19 || currentCard == 32 || currentCard == 45) {
+    return 7
+  }
+   else if (currentCard == 7 || currentCard == 20 || currentCard == 33 || currentCard == 46) {
+    return 8
+  }
+   else if (currentCard == 8 || currentCard == 21 || currentCard == 34 || currentCard == 47) {
+    return 9
+  }
+  
+   else {
+    return 10
+  }
+  
+};
+
+function game() {
+  currentCard = cardPull("p")
+  document.getElementById("pRight").src=cardLinks[currentCard];
+  pScore += cardPoints()
+  currentCard = cardPull("p")
+  document.getElementById("pLeft").src=cardLinks[currentCard];
+  pScore += cardPoints()
+  document.getElementById("pCount").innerHTML = pScore.toString();
+  currentCard = cardPull("d")
+  document.getElementById("dRight").src=cardLinks[currentCard];
+  dScore += cardPoints();
+  document.getElementById("dCount").innerHTML = dScore.toString(); 
+}
+
+function hitFunction() {
+  currentCard = cardPull("p");
+  pScore += cardPoints();
+    if (pScore > 21 && pPulledCards.includes(0) && aceUsed.includes(0) == false) {
+           pScore -= 10
+           aceUsed.push(0)
+         }
+   if (pScore > 21 && pPulledCards.includes(13) && aceUsed.includes(13) == false) {
+           pScore -= 10
+           aceUsed.push(13)
+         }
+   if (pScore > 21 && pPulledCards.includes(26) && aceUsed.includes(26) == false) {
+           pScore -= 10
+           aceUsed.push(26)
+         }
+   if (pScore > 21 && pPulledCards.includes(39) && aceUsed.includes(39) == false) {
+           pScore -= 10
+           aceUsed.push(39)
+         }
+  document.getElementById("pCount").innerHTML = pScore.toString();
+  if (extra == 0) {
+  document.getElementById("pExtra1").src=cardLinks[currentCard]
+    document.getElementById("pExtra1").style.width = "80px"
+}
+else if (extra == 1) {
+  document.getElementById("pExtra2").src=cardLinks[currentCard] 
+  document.getElementById("pExtra2").style.width = "80px"
+}
+  else if (extra == 2) {
+  document.getElementById("pExtra3").src=cardLinks[currentCard]               
+    document.getElementById("pExtra3").style.width = "80px"
+}
+  else {
+    document.getElementById("pExtra4").src=cardLinks[currentCard] 
+    document.getElementById("pExtra4").style.width = "80px"
+  }
+extra++
+  if (pScore > 21) {
+    endGame()
+  }  
+}
+
+function betButton() {
+  bet = document.getElementById("betIn").value;
+  bet = parseInt(bet)
+  if (bet > money || bet < 1 || Number.isInteger(bet) == false) {
+    alert("Invalid Bet")
+  } else {
+  document.getElementById("hit").disabled = false
+  document.getElementById("stand").disabled = false
+  document.getElementById("betIn").disabled = true
+  document.getElementById("betBut").disabled = true
+  document.getElementById("pExtra1").src=""
+  document.getElementById("pExtra1").style.width = "0px"
+  document.getElementById("pExtra2").src=""
+  document.getElementById("pExtra2").style.width = "0px"
+  document.getElementById("pExtra3").src=""
+  document.getElementById("pExtra3").style.width = "0px"
+  document.getElementById("pExtra4").src=""
+  document.getElementById("pExtra4").style.width = "0px"
+  document.getElementById("result").style.color = "transparent"
+  document.getElementById("dExtra1").src=""
+  document.getElementById("dExtra1").style.width = "0px"
+  document.getElementById("dExtra2").src=""
+  document.getElementById("dExtra2").style.width = "0px"
+  document.getElementById("dExtra3").src=""
+  document.getElementById("dExtra3").style.width = "0px"
+  document.getElementById("dExtra4").src=""
+  document.getElementById("dExtra4").style.width = "0px"
+  document.getElementById("dLeft").src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-2_1024x1024.png?v=1535755695"
+  aceUsed = []
+  pScore = 0
+  dScore = 0
+  extra = 0
+  extraD = 0
+  dPulledCards = []
+  pPulledCards = []
+  game()
+  }
+}
+
+function standFunction() {
+   currentCard = cardPull("d")
+  document.getElementById("dLeft").src=cardLinks[currentCard];
+   dScore += cardPoints();
+  document.getElementById("dCount").innerHTML = dScore.toString();
+  function add() {
+    if (dScore < 17) {
+         currentCard = cardPull("d");
+         dScore += cardPoints();
+         if (dScore > 21 && dPulledCards.includes(0) && aceUsed.includes(0) == false) {
+           dScore -= 10
+           aceUsed.push(0)
+         }
+        if (dScore > 21 && dPulledCards.includes(13) && aceUsed.includes(13) == false) {
+           dScore -= 10
+           aceUsed.push(13)
+         }
+        if (dScore > 21 && dPulledCards.includes(26) && aceUsed.includes(26) == false) {
+           dScore -= 10
+           aceUsed.push(26)
+         }
+        if (dScore > 21 && dPulledCards.includes(39) && aceUsed.includes(39) == false) {
+           dScore -= 10
+           aceUsed.push(39)
+         }
+         document.getElementById("dCount").innerHTML=dScore.toString();
+         if (extraD == 0) {
+           document.getElementById("dExtra1").src=cardLinks[currentCard]
+           document.getElementById("dExtra1").style.width = "80px"
+        }
+        else if (extraD == 1) {
+           document.getElementById("dExtra2").src=cardLinks[currentCard] 
+          document.getElementById("dExtra2").style.width = "80px"
+        }
+        else if (extraD == 2) {
+          document.getElementById("dExtra3").src=cardLinks[currentCard]               
+          document.getElementById("dExtra3").style.width = "80px"
+        }
+        else {
+           document.getElementById("dExtra4").src=cardLinks[currentCard] 
+          document.getElementById("dExtra4").style.width = "80px"
+        }
+        extraD++
+        add()
+      }
+  }
+  add()
+  endGame()
+}
+
+function againButton() {
+  money = 100
+  aceUsed = []
+  pScore = 0
+  dScore = 0
+  extra = 0
+  extraD = 0
+  dPulledCards = []
+  pPulledCards = []
+  document.getElementById("pActions").style.opacity = 1
+  document.getElementById("dLeft").src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-2_1024x1024.png?v=1535755695";
+  document.getElementById("dRight").src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-2_1024x1024.png?v=1535755695";
+  document.getElementById("pRight").src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-2_1024x1024.png?v=1535755695";
+  document.getElementById("pLeft").src="https://cdn.shopify.com/s/files/1/0200/7616/products/playing-cards-bicycle-rider-back-2_1024x1024.png?v=1535755695";
+  document.getElementById("pCount").innerHTML = "0"
+  document.getElementById("dCount").innerHTML = "0"
+  document.getElementById("betIn").value = ""
+  document.getElementById("betBut").disabled = false
+  document.getElementById("betIn").disabled = false
+  document.getElementById("money").innerHTML = "$100"
+  document.getElementById("again").style.display = "none"
+  document.getElementById("pExtra1").src=""
+  document.getElementById("pExtra1").style.width = "0px"
+  document.getElementById("pExtra2").src=""
+  document.getElementById("pExtra2").style.width = "0px"
+  document.getElementById("pExtra3").src=""
+  document.getElementById("pExtra3").style.width = "0px"
+  document.getElementById("pExtra4").src=""
+  document.getElementById("pExtra4").style.width = "0px"
+  document.getElementById("result").style.color = "transparent"
+  document.getElementById("dExtra1").src=""
+  document.getElementById("dExtra1").style.width = "0px"
+  document.getElementById("dExtra2").src=""
+  document.getElementById("dExtra2").style.width = "0px"
+  document.getElementById("dExtra3").src=""
+  document.getElementById("dExtra3").style.width = "0px"
+  document.getElementById("dExtra4").src=""
+  document.getElementById("dExtra4").style.width = "0px"
+}
+
+function endGame() {
+  if (pScore > 21 && dScore > 21) {
+    document.getElementById("result").innerHTML = "Push"
+  } 
+  if (pScore == dScore) {
+    document.getElementById("result").innerHTML = "Push"
+  }
+  else if (pScore > 21) {
+    document.getElementById("result").innerHTML = "Bust";
+    money -= bet;
+    document.getElementById("money").innerHTML = "$" + money.toString();
+  }
+  else if (dScore > 21) {
+    document.getElementById("result").innerHTML = "Dealer Busted"
+    money += +bet
+    document.getElementById("money").innerHTML = "$" + money.toString()
+  }
+  else if (pScore < dScore) {
+    document.getElementById("result").innerHTML = "Dealer Wins"
+    money -= bet
+    document.getElementById("money").innerHTML = "$" + money.toString()
+  }
+  else if (pScore > dScore) {
+    document.getElementById("result").innerHTML = "You Win!"
+    money += +bet
+    document.getElementById("money").innerHTML = "$" + money.toString()
+  }
+    document.getElementById("betBut").disabled = false
+  document.getElementById("betIn").disabled = false
+  document.getElementById("hit").disabled = true
+  document.getElementById("stand").disabled = true
+  if (money < 1) {
+    document.getElementById("result").innerHTML = "Bankrupt"
+    document.getElementById("betBut").disabled = true
+    document.getElementById("betIn").disabled = true
+    document.getElementById("again").style.display = "block"
+    document.getElementById("pActions").style.opacity = 0
+  }
+  document.getElementById("result").style.color = "black"
+}
